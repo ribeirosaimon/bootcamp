64844097812b505c91343e36b76a8c27d887ea71
Saimon <saimonribeiros@hotmail.com>
2025-06-11 14:47:02 -0300

    review1

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ribeirosaimon/bootcamp/internal/application"
+)
+
+func main() {
+	// env
+	// ...
+
+	// app
+	// - config
+	cfg := &application.ConfigServerChi{
+		ServerAddress:  ":8080",
+		LoaderFilePath: "docs/db/vehicles_100.json",
+	}
+	app := application.NewServerChi(cfg)
+	// - run
+	if err := app.Run(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
